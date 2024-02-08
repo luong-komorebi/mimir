442872458ee7137a08d17b98dbaa7e23cc4fec5b
Bryan Boreham <bjboreham@gmail.com>
2024-02-08 16:54:05 +0000

    fixup! tests: call labels.Equal because deepequal no longer works

--- pkg/storegateway/bucket_test.go
+++ pkg/storegateway/bucket_test.go
@@ -45,6 +45,7 @@ import (
 	"github.com/prometheus/prometheus/tsdb/encoding"
 	"github.com/prometheus/prometheus/tsdb/hashcache"
 	"github.com/prometheus/prometheus/tsdb/wlog"
+	"github.com/prometheus/prometheus/util/testutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/thanos-io/objstore"
@@ -1134,21 +1135,10 @@ func benchmarkExpandedPostings(
 				if !tb.IsBenchmark() {
 					seriesThatMatch := filterSeries(allSeries, testCase.matchers)
 					seriesForPostings := loadSeries(ctx, tb, p, indexr)
-					assertEqualLabelSlices(tb, seriesThatMatch, seriesForPostings)
+					testutil.RequireEqual(tb, seriesThatMatch, seriesForPostings)
 				}
 			}
 		})
-	}
-}
-
-func assertEqualLabelSlices(tb testing.TB, a, b []labels.Labels) {
-	tb.Helper()
-	assert.Equal(tb, len(a), len(b))
-	if len(a) != len(b) {
-		return
-	}
-	for i := range a {
-		assert.True(tb, labels.Equal(a[i], b[i]))
 	}
 }
 
