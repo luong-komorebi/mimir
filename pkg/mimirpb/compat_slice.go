c5f4eb760683a10de67f4895bcab925c619a2df0
Bryan Boreham <bjboreham@gmail.com>
2024-02-08 14:39:20 +0000

    fixup! mimirpb: extend compatibility to internlabels

--- pkg/mimirpb/compat_slice.go
+++ pkg/mimirpb/compat_slice.go
@@ -3,7 +3,7 @@
 // Provenance-includes-license: Apache-2.0
 // Provenance-includes-copyright: The Cortex Authors.
 
-//go:build !stringlabels && !internlabels
+//go:build !stringlabels && !dedupelabels
 
 package mimirpb
 
